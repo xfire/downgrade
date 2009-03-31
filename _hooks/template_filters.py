dbbdd2ca3545b26a19ca0284b797d1bab4b00051
Rico Schiekel <fire@downgra.de>
2009-03-31 22:38:02 +0200

    add tidy based truncate template filter to truncate xhtml correctly

--- _hooks/template_filters.py
+++ _hooks/template_filters.py
@@ -17,3 +17,16 @@ def xmldatetime(dt):
     if tz:
         zprefix = tz[:3] + ":" + tz[3:]
     return dt.strftime("%Y-%m-%dT%H:%M:%S") + zprefix
+
+
+@templateFilter
+def xtruncate(s, length=255, end='...'):
+    import tidy
+
+    options = dict(output_xhtml=1,
+                   add_xml_decl=1,
+                   indent=1,
+                   show_body_only=1,
+                   tidy_mark=0)
+    return str(tidy.parseString(str(s[:length]), **options)) + end
+
