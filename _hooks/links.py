e52ce62f859feb93747b0a009047698f289daa1d
Rico Schiekel <fire@downgra.de>
2009-03-31 22:37:20 +0200

    add project urls

--- _hooks/links.py
+++ _hooks/links.py
@@ -9,6 +9,14 @@ class Link(object):
         self.url = url
 
 
+projects = [ Link('growl',       'https://github.com/xfire/growl/tree'),
+             Link('pydzen',      'https://github.com/xfire/pydzen/tree'),
+             Link('python-wmii', 'https://github.com/xfire/python-wmii/tree'),
+             Link('preplace',    'https://github.com/xfire/preplace/tree'),
+             Link('jitertools',  'https://github.com/xfire/jitertools/tree'),
+             Link('mailfilter',  'http://projects.spamt.net/mailfilter/'),
+           ]
+
 links = [ Link('github',             'http://github.com/xfire/'),
           Link('dotfiles',           'https://github.com/xfire/dotfiles/tree'),
           Link("copton's blog",      'http://blog.copton.net/'),
@@ -17,7 +25,9 @@ links = [ Link('github',             'http://github.com/xfire/'),
           Link("freaky's blog",      'http://www.freakysoft.de/blog/'),
         ]
 
+updates = AttrDict(projects = projects, links = links)
+
 if not 'site' in Site.CONTEXT:
-    Site.CONTEXT.site = AttrDict(links = links)
+    Site.CONTEXT.site = updates
 else:
-    Site.CONTEXT.site.links = links
+    Site.CONTEXT.site.update(updates)
